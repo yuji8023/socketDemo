3779f467c8161114d2238f8b116d35a3188e6daf
hukangwei <hukangwei@zhuojianchina.com>
2017-07-07 17:07:43 +0800

    new socket demo

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# socketDemo
+# socket-demo
 
 个人写的一个小demo，用于研究socket.io
 
--- app.js
+++ app.js
@@ -1,49 +1,22 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var app = require('express')();
+var http = require("http").Server(app);
+var io = require('socket.io')(http);
 
+app.get("/",function(req,res){
+	res.sendFile(__dirname + '/index.html');
+})
 
-app.listen(3000);
-
-function handler(req, res) {
-	fs.readFile(__dirname + '/index.html',
-		function(err, data) {
-			if (err) {
-				res.writeHead(500);
-				return res.end('Error loading index.html');
-			}
-
-			res.writeHead(200);
-			res.end(data);
-		});
-}
-
-io.sockets.on('connection', function(socket) {
-	// socket.emit('news', { hello: 'world' });
-	// socket.on('my other event', function (data) {
-	//   console.log(data);
-	// });
-
-	socket.set('nickname', 'nickname', function() {});
-
-	socket.on('set nickname', function(name) {
-		socket.set('nickname', name, function() {
-			console.log("change nickname=>" + name);
-			socket.emit('nickname ready');
-		});
-	});
-
-	socket.on('chat', function(data) {
-		socket.get('nickname', function(err, name) {
-
-			socket.emit("notice", {
-				message: name + ":" + data.message
-			});
-			socket.broadcast.emit("notice", {
-				message: name + ":" + data.message
-			});
-			console.log("chat: " + data.message);
-		});
-	});
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+  socket.on('chat message', function(msg){
+  	console.log(msg);
+    io.emit('chat message', msg);
+  });
+  
 });
+
+http.listen(3000,function(){
+	console.log("server running on 3000");
+})
+
 
